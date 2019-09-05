7caeed64cef1106ae45a4764541177ca6e63e546
Rhythm Vashist <rhythm@harman.localdomain>
2019-09-05 15:30:23 -0700

    stating with the templating

--- polls/urls.py
+++ polls/urls.py
@@ -2,8 +2,19 @@ from django.conf.urls import url
 from . import  views
 
 urlpatterns=[
-    url('',views.deft,name='defaultpolls'),
+
+    #this is for polls/
     url(r'^$',views.index,name='index'),
-    url(r'^$',views.visit,name='visit'),
-    url(r'^$',views.index,name='index'),
-    ]
+
+    # polls/5
+    url(r'^(?P<question_id>[0-9]+)/$',views.detail,name='detail'),
+
+    #name/5/result
+
+    url(r'^(?P<question_id>[0-9]+)/result/$',views.result,name='result'),
+
+    #name/5/vote
+
+    url(r'^(?P<question_id>[0-9]+)/vote/$',views.vote,name='vote'),
+
+]
--- polls/views.py
+++ polls/views.py
@@ -3,12 +3,27 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import  HttpResponse
+from .models import Question
 
 def deft(requet):
     return HttpResponse("inside polls ")
 
 def index(request):
-    return HttpResponse("hello my name is phase 1 project ")
+    ques_list=Question.objects.order_By('-pub_date')[:5]
+    output=','.join([q.question_text for q in ques_list])
+    return HttpResponse(output)
+
 
 def visit(request):
     return HttpResponse("hey you are visting the phase 1 project")
+
+def detail(request,question_id):
+    return HttpResponse('YOUR ARE LOOKIN AT QUESTIONS %s.'% question_id)
+
+def result(request,question_id):
+    return HttpResponse('your are looking at the result of the question %s '% question_id)
+
+
+def vote (request,question_id):
+    return HttpResponse('your are voting on question %s'% question_id)
+
